e5f0ae03966507d297ff0d37d1d3ceffa3ad80a7
pranave02 <pranavekarthik@gmail.com>
2022-07-22 13:19:29 +0530

    [jersey implementation]

---
+++ maintenance-app-jersey/src/main/java/com/pranave/maintenance_app/test.java
@@ -0,0 +1,19 @@
+package com.pranave.maintenance_app;
+@jakarta.xml.bind.annotation.XmlRootElement
+public class test {
+	private int id;
+	private int flat_no;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getFlat_no() {
+		return flat_no;
+	}
+	public void setFlat_no(int flat_no) {
+		this.flat_no = flat_no;
+	}
+	
+}
---
+++ maintenance-app-jersey/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+    <h2>Flats management</h2>
+    <p><a href="webapi/owner_info/ownerflats">owner_info</a>
+    <p><a href="webapi/owner_info/ownerflats/maintenance/yes">Maintenace_paid</a>
+    <p><a href="webapi/owner_info/ownerflats/maintenance/no">Maintenace_notpaid</a>
+    <p><a href="webapi/owner_info/ownerflats/vacent">Vacent_flats</a>
+</body>
+</html>
